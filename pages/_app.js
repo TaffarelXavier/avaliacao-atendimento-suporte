33fd2f8ceec6117e84994544bb8d376a0b9a7984
Taffarel Xavier <taffarel_deus@hotmail.com>
2020-07-08 12:21:11 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,46 @@
+import React from "react";
+import App from "next/app";
+import "react-rater/lib/react-rater.css";
+import { PageTransition } from "next-page-transitions";
+
+class MyApp extends App {
+	
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+    return (
+      <>
+        <PageTransition timeout={300} classNames="page-transition">
+          <Component {...pageProps} key={router.route} />
+        </PageTransition>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
+      </>
+    );
+  }
+}
+
+export default MyApp;
